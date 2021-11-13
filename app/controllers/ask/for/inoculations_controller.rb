4b859979008ade75b63a08893fd72fbeac0687af
Mathieu Lovato Stumpf Guntz <psychoslave@culture-libre.org>
2021-11-13 07:53:00 +0100

    Move vaccination report generation in Inoculation model

--- app/controllers/ask/for/inoculations_controller.rb
+++ app/controllers/ask/for/inoculations_controller.rb
@@ -2,44 +2,7 @@ module Ask
   module For
     class InoculationsController < ApplicationController
       def index
-        fad = inoculation_params
-
-        # TODO: move all this logic to the model
-        country = fad[:country] = Country
-          .includes(vaccines: :countries).where(reference: fad[:country]).first
-
-        unless fad[:user].nil?
-          inoculations = country.inoculations.where(fad).order(appointement_at: :desc)
-        end
-        fulfilled_inoculations = inoculations.where(fulfilled: true)
-
-        aim = %w[name reference composition]
-        vaccines = country.vaccines.map do |vaccine|
-          wad = inoculations
-            .select{ _1.vaccine_id == vaccine.id }
-
-          # TODO, consider further optimization with a counter cache
-          locally_fulfilled_inoculations = wad.nil? ? 0 : wad.count
-          rate = 100.0 * locally_fulfilled_inoculations / country.population
-          tut = {
-            vaccine: vaccine.attributes.select{ |a| a.in? aim },
-            delivering_countries: vaccine.countries.map(&:name),
-            country_population: country.population,
-            locally_fulfilled_inoculations: locally_fulfilled_inoculations,
-            coverage: rate,
-          }
-          # next vaccination booster date if user was provided
-          # TODO, add filter to take into account only dates still in future?
-          unless fad[:user].nil?
-            tut[:next_appointment] = inoculations
-              .select{ _1.vaccine_id == vaccine.id }.first
-              &.appointement_at&.iso8601
-          end
-
-          tut
-        end
-
-        render json: vaccines.to_json
+        render json: Inoculation.report(report_params).to_json
       end
 
       # GET /inoculations/new
@@ -78,10 +41,10 @@ module Ask
       end
 
       protected
-        # Only allow a list of trusted parameters through.
-        def inoculation_params
+        # Ensure that country is present, and allow it together with user
+        def report_params
           params.require(:country)
-          params.permit(:country, :inoculation, :user)
+          params.permit(:country, :user)
         end
     end
   end
--- app/models/inoculation.rb
+++ app/models/inoculation.rb
@@ -1,4 +1,45 @@
 class Inoculation < ApplicationRecord
   belongs_to :vaccine
   belongs_to :country
+
+  # Generate a report of vaccination status for the sought criteria
+  #
+  # sought:: a hash table providing desired criteria to apply
+  # +sought[:country]+:: mandatory, the country to target in this report
+  # +sought[:user]+:: optional, if provided, used to fill a +next_appointment+ attribute
+  def self.report(sought)
+    country = sought[:country] = Country
+      .includes(vaccines: :countries).where(reference: sought[:country]).first
+
+    unless sought[:user].nil?
+      inoculations = country.inoculations.where(sought).order(appointement_at: :desc)
+    end
+    fulfilled_inoculations = inoculations.where(fulfilled: true)
+
+    swab = %w[name reference composition]
+    country.vaccines.map do |vaccine|
+      wad = inoculations
+        .select{ _1.vaccine_id == vaccine.id }
+
+      # TODO, consider further optimization with a counter cache
+      locally_fulfilled_inoculations = wad.nil? ? 0 : wad.count
+      rate = 100.0 * locally_fulfilled_inoculations / country.population
+      tut = {
+        vaccine: vaccine.attributes.select{ |a| a.in? swab },
+        delivering_countries: vaccine.countries.map(&:name),
+        country_population: country.population,
+        locally_fulfilled_inoculations: locally_fulfilled_inoculations,
+        coverage: rate,
+      }
+      # next vaccination booster date if user was provided
+      # TODO, consider filtering outdated appointements
+      unless sought[:user].nil?
+        tut[:next_appointment] = inoculations
+          .select{ _1.vaccine_id == vaccine.id }.first
+        &.appointement_at&.iso8601
+      end
+
+      tut
+    end
+  end
 end
